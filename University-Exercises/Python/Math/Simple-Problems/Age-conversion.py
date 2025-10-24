c5c1d4dd163964482454fa73179961f15fa31ac9
Sasan <sasansafari94@gmail.com>
2025-10-24 15:43:24 +0330

    Update Age-conversion.py

--- University-Exercises/Python/Math/Simple-Problems/Age-conversion.py
+++ University-Exercises/Python/Math/Simple-Problems/Age-conversion.py
@@ -6,7 +6,7 @@ try:
     age_hour = age_day*24
     age_minute = age_hour*60
     age_second = age_minute*60
-    print(f"{age} year{'s' if age > 0 else ''} in months is {age_month} \n in days is {age_day:,} \n in hours is {age_hour:,} \n in minutes is {age_minute:,} \n in seconds is {age_second:,}")
+    print(f"{age} year{'s' if age > 1 else ''} in months is {age_month} \n in days is {age_day:,} \n in hours is {age_hour:,} \n in minutes is {age_minute:,} \n in seconds is {age_second:,}")
   else:
     print("Enter a number greater than 0 Please")
 except ValueError:
