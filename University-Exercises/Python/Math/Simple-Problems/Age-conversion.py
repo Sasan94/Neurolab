0bfe9dc7861b1c3d5d86f4b30a4542a56ac5982f
Sasan <sasansafari94@gmail.com>
2025-10-24 14:44:37 +0330

    Add Age-Conversion
    This exercise can take the user's age in years and convert it into months, days, hours, minutes, and seconds.

---
+++ University-Exercises/Python/Math/Simple-Problems/Age-conversion.py
@@ -0,0 +1,13 @@
+try:
+  age = int(input("Type your age in years here: "))
+  if age > 0:
+    age_month = age*12
+    age_day = age_month*30
+    age_hour = age_day*24
+    age_minute = age_hour*60
+    age_second = age_minute*60
+    print(f"{age} year{'s' if age !=0 else ''} in months is {age_month} \n in days is {age_day:,} \n in hours is {age_hour:,} \n in minutes is {age_minute:,} \n in seconds is {age_second:,}")
+  else:
+    print("Enter a number greater than 0 Please")
+except ValueError:
+  print("Enter numbers only")
