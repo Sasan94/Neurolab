999e2afd8d9c5be8d962451d5fcc55526803320d
Sasan <sasansafari94@gmail.com>
2025-10-24 18:15:48 +0330

    Updated Simple-Game.py
    This exercise uses while loop to create a simple game using two options left or right.

--- University-Exercises/Python/Basics/Simple-Game.py
+++ University-Exercises/Python/Basics/Simple-Game.py
@@ -1,6 +1,15 @@
+# ask the user to go left or right
 where = input("Take left or right: ")
+
+#to count attempts that user make
 attempt = 0
+
+#This condition cheks that user will not take the wrong option
 while where != "right":
    where = input("Try again: ")
+
+   #Each failure will be added to the counter 
    attempt += 1
+
+#This prints how many attempts you've made to find the right option so far. 
 print (f"You Got Out after {attempt+1} attempt{'s' if attempt > 1 else ''}")
